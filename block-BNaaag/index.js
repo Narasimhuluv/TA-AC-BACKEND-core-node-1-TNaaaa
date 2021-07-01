161d27439364dcf7e094790b7c0bcf43824d9974
Narasimhuluv <vasamnarasimhulu123@gmail.com>
2021-07-01 11:09:02 +0530

    added files

---
+++ block-BNaaag/index.js
@@ -0,0 +1,10 @@
+let fileSy = require('fs');
+
+fileSy.readFile('./index.md', (err, content) => {
+  console.log(err, content.toString());
+});
+
+let buffer1 = Buffer.alloc(10);
+console.log(buffer1);
+buffer1.write('Welcome to Buffer');
+console.log(buffer1.toString());
