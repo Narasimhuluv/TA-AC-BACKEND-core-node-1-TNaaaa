d180a06511d1c171703cf4cb36ff8ac052c60a62
Narasimhuluv <vasamnarasimhulu123@gmail.com>
2021-07-01 11:39:09 +0530

    added files

--- block-BNaaag/index.js
+++ block-BNaaag/index.js
@@ -1,9 +1,12 @@
 let fileSy = require('fs');
+let fileSyAsync = require('fs');
 
 fileSy.readFile('./content.md', (err, content) => {
   console.log(err, content.toString());
 });
 
+let results = fileSyAsync.readFileSync('./content.md', 'utf-8');
+console.log(results);
 let buffer1 = Buffer.alloc(10);
 console.log(buffer1);
 buffer1.write('Welcome to Buffer');
