75e7fe1f15a4f7b1a11fcfdb096ed06f496c5a92
Narasimhuluv <vasamnarasimhulu123@gmail.com>
2021-07-02 08:45:14 +0530

    added files

---
+++ block-BNaaat/index.js
@@ -0,0 +1,39 @@
+// let http = require('http');
+// let file = require('fs');
+// let server = http.createServer(handleRequest);
+
+// function handleRequest(req, res) {
+//   console.log(req.method, req.url);
+//   if (req.method === 'GET' && req.url === '/') {
+//     res.setHeader('Content-Type', 'text/html');
+//     res.end('<h1> Welcome to the node files</h1>');
+//   } else if (req.method === 'GET' && req.url === '/node') {
+//     res.setHeader('Content-Type', 'text/html');
+//     file.readFile('./node.html', 'utf8', (err, content) => {
+//       if (err) console.log(err);
+//       res.end(content);
+//     });
+//   }
+// }
+// server.listen(3000, () => {
+//   console.log('Server port is running');
+// });
+
+let filesystem = require('fs');
+let http = require('http');
+let server = http.createServer(handleRequest);
+function handleRequest(req, res) {
+  console.log(req.method, req.url);
+  if (req.method === 'GET' && req.url === '/') {
+    res.setHeader('content-type', 'text/html');
+    res.end('<h1> Welcome to the node file</h1>');
+  } else if (req.method === 'GET' && req.url === '/node') {
+    res.writeHead(201, { 'content-type': 'text/html' });
+    filesystem.createReadStream('./node.html').pipe(res);
+    // res.end();
+  }
+}
+
+server.listen(3000, () => {
+  console.log('Server Port is Running Fine');
+});
